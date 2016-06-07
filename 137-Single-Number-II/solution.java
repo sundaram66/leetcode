b8115a6eeb498d71dc3bb17e4b29ceb9eb51734b
Seetharam <seetharam.sundaram@imaginea.com>
2016-06-07 18:05:05 +0530

    [137. Single Number II][Accepted]committed by LeetCode Extension

--- 137-Single-Number-II/solution.java
+++ 137-Single-Number-II/solution.java
@@ -1,14 +1,14 @@
 public class Solution {
     public int singleNumber(int[] nums) {
-        
-        if(nums.length == 0) return -1;
-        if(nums.length == 1 || nums.length == 2) return nums[0]; 
+        int n = nums.length;
+        if(n == 0) return -1;
+        if(n == 1 || n == 2) return nums[0]; 
         int result = 0;
         
         Arrays.sort(nums);
         
-        for(int i=0;i<nums.length;) {
-           if(i+1 == nums.length) return nums[i];
+        for(int i=0;i<n;) {
+           if(i+1 == n) return nums[i];
            if(nums[i] != nums[i+1]) return nums[i];
            i += 3;
         }
