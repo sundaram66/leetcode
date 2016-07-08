177a0f01be518a0f5a9b4653a8452723aa21cba0
Seetharam <seetharam.sundaram@imaginea.com>
2016-07-08 17:50:01 +0530

    [283. Move Zeroes][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 283-Move-Zeroes/solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public void moveZeroes(int[] a) {
+        
+        int n = a.length;
+        if(n==0) return;
+        
+        int i=0,j=0;
+        
+        while(true) {
+          
+          while(i<n && a[i] != 0) { i++; }
+          while(j<n && a[j] == 0) { j++; }
+          
+          
+          
+          if(i<n && j<n && i<j) {
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            i++;
+          }
+          
+          j++;
+          
+          
+          if(i == n || j==n) return;
+          
+          
+        }
+        
+        
+            
+    
+        
+    }
+}
