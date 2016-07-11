93f1a9d8f2631c845e6b874956502cf5416d0732
Seetharam <seetharam.sundaram@imaginea.com>
2016-07-11 12:54:23 +0530

    [258. Add Digits][Accepted]committed by LeetCode Extension

--- 258-Add-Digits/solution.java
+++ 258-Add-Digits/solution.java
@@ -2,16 +2,11 @@ public class Solution {
     public int addDigits(int num) {
         
         if(num == 0 || num < 10) return num;
-        int sum = 0;
+       
         
-        while(num > 0) {
-             sum += num%10;
-             num = num/10;
-             
-             if(num == 0 && sum >=10)  { num = sum; sum = 0; }
-        }
+        // Without loop, Digital roots of number : https://en.wikipedia.org/wiki/Digital_root
         
-        return sum;
+        return num%9==0?9:num%9;
         
     }
 }
