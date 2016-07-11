19b99a8b02dbd679c1723c7b1682202865ef3f9f
Seetharam <seetharam.sundaram@imaginea.com>
2016-07-11 12:52:45 +0530

    [258. Add Digits][Accepted]committed by LeetCode Extension

--- 258-Add-Digits/solution.java
+++ 258-Add-Digits/solution.java
@@ -8,7 +8,7 @@ public class Solution {
              sum += num%10;
              num = num/10;
              
-             if(num == 0 && sum >=10) num = sum;
+             if(num == 0 && sum >=10)  { num = sum; sum = 0; }
         }
         
         return sum;
