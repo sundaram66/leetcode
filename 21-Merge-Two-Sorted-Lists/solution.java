0d1809335588a0ba6af00429d09fdad688fa0912
Seetharam <seetharam.sundaram@imaginea.com>
2016-08-17 15:27:56 +0530

    [21. Merge Two Sorted Lists][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 21-Merge-Two-Sorted-Lists/solution.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        
+        ListNode head = l1;
+        
+        ListNode currL1 = l1;
+        ListNode currL2 = l2;
+        
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        
+        if(l1.val > l2.val) {
+            currL2 = l2;
+            l2 = l2.next;
+            currL2.next = l1;
+            head = currL2;
+        }
+        
+        ListNode prevL1 = l1;
+
+        while(l1 != null && l2 != null) {
+            
+
+            currL2 = l2;
+
+            if(l1.val <= l2.val) {
+                prevL1 = l1;
+                l1 = l1.next;
+            }
+            
+            else {
+                prevL1.next = l2;
+                l2 = l2.next;
+                currL2.next = l1;
+            }
+        }
+        
+        if(l1 == null) 
+             prevL1.next = l2;
+             
+        return head;
+    }
+    
+    
+    
+}
