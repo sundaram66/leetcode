ba06bacd4e314c3d7be15c41007cc7684dc2619b
Seetharam <seetharam.sundaram@imaginea.com>
2016-08-17 13:27:26 +0530

    Update solution.java

--- 141-Linked-List-Cycle/solution.java
+++ 141-Linked-List-Cycle/solution.java
@@ -9,6 +9,9 @@
  *     }
  * }
  */
+ 
+ // http://stackoverflow.com/questions/2936213/explain-how-finding-cycle-start-node-in-cycle-linked-list-work
+ // https://www.quora.com/Why-we-increment-the-fast-pointer-by-two-cant-we-increment-it-by-3-or-4-and-slow-pointer-by-one-to-find-the-existence-of-loop-in-a-singly-linked-list-Any-proof-link-will-be-appreciated
 public class Solution {
     public boolean hasCycle(ListNode head) {
         
