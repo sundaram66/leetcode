8cd8fa88959c5f14bf7ec942e3e0c85c540af1a5
Seetharam <seetharam.sundaram@imaginea.com>
2016-09-19 15:06:31 +0530

    [55. Jump Game][Accepted]committed by LeetCode Extension

--- 55-Jump-Game/solution.java
+++ 55-Jump-Game/solution.java
@@ -2,23 +2,17 @@ public class Solution {
     public boolean canJump(int[] nums) {
         
         int n = nums.length;
-        
-        int[] dp = new int[n];
-        
-        dp[n-1] = 1;
-        
-        for(int i=n-2;i>=0;i--) {
-            
-            for(int j=i+1;j<=nums[i]+i;j++) {
-                if(j >= n || dp[j] == 1) {
-                    dp[i] = 1;
-                    break;
-                }
-            }
+
+        int maxIndex = nums[0];
+
+        for(int i=0;i<n;i++) {
+            if(maxIndex >= n) return true;
+            if(i > maxIndex) break;
+            maxIndex = Math.max(maxIndex,i+nums[i]);
         }
         
-        if(dp[0] == 1)
-            return true;
+        if(maxIndex >= n-1) return true;
+        if(maxIndex == 0 && n == 1) return true;
         return false;
         
     }
