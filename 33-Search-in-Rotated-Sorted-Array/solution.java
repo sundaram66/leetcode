a9e3c08c2fa4fc07f310b67e5c7d8e7d5ac17c05
Seetharam <seetharam.sundaram@imaginea.com>
2016-10-05 14:53:46 +0530

    [33. Search in Rotated Sorted Array][Time Limit Exceeded]committed by LeetCode Extension

--- 33-Search-in-Rotated-Sorted-Array/solution.java
+++ 33-Search-in-Rotated-Sorted-Array/solution.java
@@ -1,38 +1,31 @@
 public class Solution {
-    public int search(int[] a, int target) {
+    public int search(int[] nums, int target) {
         
-        int left = 0;
-        int right = a.length-1;
-        
-        if(a[0] == target) return 0;
-        
-        while(left < right) {
-            
-            if(a[left] == target) return left;
-            if(a[right] == target) return right;
-            
-            int mid = left+(right-left)/2;
-            
-            if(a[mid] == target) return mid;
-            
-            
-            
-            if(a[mid] > a[left]) { 
-                if(a[mid] > target && target >= a[left])
-                    right = mid-1;
-                else
-                   left = mid+1;
+        int low = 0, high = nums.length;
+        while (low < high) {
+            int mid = (low + high) >>> 1;
+            if (nums[low] < nums[high - 1]) {
+                if (target > nums[mid]) low = mid + 1;
+                else if (target < nums[mid]) high = mid;
+                else return mid;
+            } else {
+                if (nums[mid] >= nums[low]) {
+                    if (target > nums[mid]) low = mid + 1;
+                    else if (target < nums[mid])
+                        if (target >= nums[low]) high = mid;
+                        else if (target <= nums[high - 1]) low = mid + 1;
+                        else return -1;
+                    else return mid;
+                } else if (nums[mid] <= nums[low]) {
+                    if (target < nums[mid]) high = mid;
+                    else if (target > nums[mid])
+                        if (target <= nums[high - 1]) low = mid + 1;
+                        else if (target >= nums[low]) high = mid;
+                        else return -1;
+                    else return mid;
+                }
             }
-            
-            else {
-                if(a[mid] < target && target <= a[right])
-                    left = mid+1;
-                else
-                    right = mid-1;
-            }
-            
         }
-        
         return -1;
         
     }
